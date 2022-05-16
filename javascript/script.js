a181dc90746ed619b7da873f70deb3706c599f2f
Nympheuel <fabiesmegar@gmail.com>
2022-05-15 23:43:31 -0300

    intento de boton menu con java script

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+jQuery('document').ready(function($){
+    var menuBtn= $('.menu-icon'),
+        menu = $('.navegacion ul');
+
+    menuBtn.click(function() {
+        menu.addClass('show');
+});   
+});
