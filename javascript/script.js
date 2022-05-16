73bbf44b1d911e466cc35226ef20a854d6efaca8
Nympheuel <fabiesmegar@gmail.com>
2022-05-16 00:57:35 -0300

    ver menu modificado y comentado (Fabi)

--- javascript/script.js
+++ javascript/script.js
@@ -2,14 +2,15 @@
 Funciona cuando el navegador abre nuestro documento
 */
 jQuery('document').ready(function($) {
-	var menuBtn = $('.menu-icon'), /* creacion de var menu-icon*/
+	var menuBtn = $('.menu-icon') /* creacion de var menu-icon*/,
 		menu = $('.navegacion ul'); /* creacion de var que sera el menu*/
 
-	menuBtn.click(function() { /* condicional que sucede al hacer click en icono menu*/
-		if (menu.hasClass('show')) { /* Si menu tiene la clase "show"*/
+	menuBtn.click(function() {
+		/* condicional que sucede al hacer click en icono menu*/
+		if (menu.hasClass('show')) {
+			/* Si menu tiene la clase "show"*/
 
 			menu.removeClass('show'); /* sacar la clase "show", esto hara que no se vea de una vez*/
-
 		} else {
 			menu.addClass('show'); /* sino, se la añade para poder ver el menu*/
 		}
