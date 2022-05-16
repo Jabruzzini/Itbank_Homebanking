7bf5578df9eddc9ce37e2a9a4d5877e89aa3ee99
Nympheuel <fabiesmegar@gmail.com>
2022-05-16 00:43:23 -0300

    boton menu funcional

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,14 @@
-jQuery('document').ready(function($){
-    var menuBtn= $('.menu-icon'),
-        menu = $('.navegacion ul');
+jQuery('document').ready(function($) {
+	var menuBtn = $('.menu-icon'),
+		menu = $('.navegacion ul');
 
-    menuBtn.click(function() {
-        menu.addClass('show');
-});   
+	menuBtn.click(function() {
+		if (menu.hasClass('show')) {
+
+			menu.removeClass('show');
+            
+		} else {
+			menu.addClass('show');
+		}
+	});
 });
