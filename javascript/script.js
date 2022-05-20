9d794da6c2b416795caeb92e2b54b8aee0d31e5a
Nympheuel <fabiesmegar@gmail.com>
2022-05-19 21:26:19 -0300

    terminado

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,11 @@
 /*Funcion Mostrar menu de comprobantes
-Funciona cuando el navegador abre nuestro documento
-*/
-jQuery('document').ready(function($) {
-	var menuBtn = $('.menu-icon'), /* creacion de var menu-icon*/
-		menu = $('.navegacion ul'); /* creacion de var que sera el menu*/
+Funciona cuando el navegador abre nuestro documento*/
 
-	menuBtn.click(function() {
-		/* condicional que sucede al hacer click en icono menu*/
-		if (menu.hasClass('show')) {
-			/* Si menu = .navigation ul tiene la clase "show"*/
+ $('document').ready(function() {
+	if(window.innerWidth < 768){
+		$('.table').addClass('pesos');
+	}else{
+		$('.table').removeClass('pesos')
+	}
+ });
 
-			menu.removeClass('show'); /* sacar la clase "show", esto hara que no se vea de una vez*/
-		} else {
-			menu.addClass('show'); /* sino, se la añade para poder ver el menu*/
-		}
-	});
-});
